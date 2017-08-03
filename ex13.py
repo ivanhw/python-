6f1ae0691ff6352db8d917392087897c9e33601e
何炜 <30584259+ivanhw@users.noreply.github.com>
2017-08-03 23:44:08 +0800

    我这不是零基础，是负基础。

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, things, that = argv
+
+print "The script is called:", script 
+print "Your first variable is:", first 
+print "Your second variable is:", things
+print "Your third variable is:", that
